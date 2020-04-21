1ea9e417cf19aeacdbd3f7dcbb0e365b7e4f8050
unknown <longnvt24111999@gmail.com>
2020-04-22 00:47:21 +0700

    addfile

---
+++ Công nghệ phần mềm 5/Công nghệ phần mềm 5/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Công_nghệ_phần_mềm_5
+{
+    public partial class Form1 : Form
+    {
+        int dem = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+            textBox2.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void dangnhap()
+        {
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 fm = new Form2();
+            if (textBox1.Text == "QuanLy" && textBox2.Text == "123456")
+            {
+                MessageBox.Show("Đăng nhập thành công", "Thông báo");
+                fm.ShowDialog();
+            }
+            else
+            {
+                dem++;
+                MessageBox.Show("Bạn đã nhập sai tài khoản hoặc mật khẩu", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (dem==3)
+            {
+                MessageBox.Show("Bạn đã nhập sai quá nhiều lần", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Enabled = false;
+                textBox1.Text = "";
+                textBox2.Enabled = false;
+                textBox2.Text = "";
+            }
+        }
+    }
+}
---
+++ Công nghệ phần mềm 5/Công nghệ phần mềm 5/Form2.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Công_nghệ_phần_mềm_5
+{
+    public partial class Form2 : Form
+    {
+        int them = 0;
+        int sua = 0;
+        int xoa = 0;
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-S78DLEL\SQLEXPRESS01;Initial Catalog=QuanLyNhanVien;Integrated Security=True");
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            them = 1;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+        public void themnv()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into NhanVien values('" + textBox1.Text + "',N'" + textBox2.Text + "',N'" + textBox3.Text + "','" + textBox4.Text + "',N'" + textBox5.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            disp_data();
+            MessageBox.Show("Bạn Thêm Thành Công");
+        }
+        public void disp_data()
+        {
+            con.Open();
+            string sql = "select * from NhanVien";  // lay het du lieu trong bang NhanVien
+            SqlCommand cmd = new SqlCommand(sql, con); //bat dau truy van
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(cmd); //chuyen du lieu ve
+            DataTable dt = new DataTable(); //tạo một kho ảo để lưu trữ dữ liệu
+            da.Fill(dt);  // đổ dữ liệu vào kho
+            dataGridView1.DataSource = dt; //đổ dữ liệu vào datagridview
+            con.Close();  // đóng kết nối
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            disp_data();
+        }
+        public void xoanv()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete NhanVien where MaNV= " + textBox1.Text + "";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            disp_data();
+            MessageBox.Show("Bạn Xóa Thành Công");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            xoa = 1;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        public void suavn()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update NhanVien set HoTen= N'" + textBox2.Text + "',DiaChi= N'" + textBox3.Text + "',SDT='" + textBox4.Text + "',Email=N'" + textBox5.Text + "'where MaNV='" + textBox1.Text + "' ";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            disp_data();
+            MessageBox.Show("Bạn Sửa Thành Công");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sua = 1;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string sql = "select * from NhanVien where MaNV=" + textBox6.Text + "";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(them==1)
+            {
+                themnv();
+            }
+            if(sua==1)
+            {
+                suavn();
+            }   
+            if(xoa==1)
+            {
+                xoanv();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;
+            if (index >=0)
+            {
+                textBox1.Text = (dataGridView1.Rows[index].Cells[0].Value.ToString());
+                textBox2.Text = (dataGridView1.Rows[index].Cells[1].Value.ToString());
+                textBox3.Text = (dataGridView1.Rows[index].Cells[2].Value.ToString());
+                textBox4.Text = (dataGridView1.Rows[index].Cells[3].Value.ToString());
+                textBox5.Text = (dataGridView1.Rows[index].Cells[4].Value.ToString());
+            }
+        }
+    }
+}
